b5dd1483be0dd522493ee846b2f506843bcf45ef
iamcharlie17 <riyadhosen40@gmail.com>
2024-10-07 08:59:23 +0600

    add: rectangle class

--- src/src/Rectangle.java
+++ src/src/Rectangle.java
@@ -1,4 +1,16 @@
 package src;
 
-public class Rectangle {
+public class Rectangle extends CommonShape{
+    private int width, height;
+
+    public Rectangle(int x, int y, int width, int height) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
 }

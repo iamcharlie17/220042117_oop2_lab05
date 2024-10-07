2f16e3411c71f211195594cb5eab8735d85d726a
iamcharlie17 <riyadhosen40@gmail.com>
2024-10-07 08:57:20 +0600

    add: Square class

--- src/src/Square.java
+++ src/src/Square.java
@@ -1,4 +1,16 @@
 package src;
 
-public class Square {
+public class Square extends CommonShape {
+
+    private int sideLength;
+
+    public Square(int x, int y, int sideLength) {
+        super(x, y);
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double calculateArea() {
+        return sideLength * sideLength;
+    }
 }
